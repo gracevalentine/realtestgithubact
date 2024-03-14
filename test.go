9f5bbe711dcd7557f19ac367a2add2f89de81842
gracevalentine <21gra.ce04@gmail.com>
2024-03-14 20:36:53 +0700

    commit kedua

--- test.go
+++ test.go
@@ -6,9 +6,9 @@ import (
 
 // nomor 1C
 var (
-	var_1 float32 = 20.5 // yang ini private, ditandai dgn huruf bukan kapital pd awal var
-	Var_2 float3  = 20.5 // yang ini public, ditandai dgn huruf kapital pd awal var
-	num   int
+	var1 float32 = 20.5 // yang ini private, ditandai dgn huruf bukan kapital pd awal var
+	Var2 float3  = 20.5 // yang ini public, ditandai dgn huruf kapital pd awal var
+	num  int
 )
 
 func main() {
