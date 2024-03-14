102a22b86872a0f24c1d84fdc06e996f58dc4da7
gracevalentine <21gra.ce04@gmail.com>
2024-03-14 23:29:27 +0700

    commit ke 11

--- test.go
+++ test.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-var (
-	var1 float32 = 99999999
-	Var2 float32 = 30000 
-	num  int
-)
+// var (
+// 	var1 float32 = 99999999
+// 	Var2 float32 = 30000 
+// 	num  int
+// )
 
 func main() {
 	fmt.Print("semoga jalan yaa")
