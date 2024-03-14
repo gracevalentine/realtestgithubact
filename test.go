1e824d85223cbc55e0482cc5455234dce12a22b3
gracevalentine <21gra.ce04@gmail.com>
2024-03-14 21:30:57 +0700

    commit ketujuh

--- test.go
+++ test.go
@@ -7,12 +7,13 @@ import (
 // nomor 1C
 var (
 	var1 float32 = 20.5 // yang ini private, ditandai dgn huruf bukan kapital pd awal var
+	Var2 float32 = 20.5 // yang ini public, ditandai dgn huruf kapital pd awal var
 	num  int
 )
 
 func main() {
 	fmt.Print("anjing jalan")
-	for i := 1; i <= num; i++ {
+	for i := 1; i < num; i++ {
 		fmt.Print("*")
 	}
 }
